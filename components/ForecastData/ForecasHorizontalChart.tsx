00bfc2289af7ba2b3b767665b15723b517ef7d90
calher99 <calher99@gmail.com>
2023-08-15 14:52:52 +0100

    add styling to Horizontal for ecast

---
+++ components/ForecastData/ForecasHorizontalChart.tsx
@@ -0,0 +1,75 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { ForecastHourData } from "../../types/forecast";
+import { Colors } from "../../constants/styles";
+
+const ForecasHorizontalChart = ({
+  dataPoints,
+}: {
+  dataPoints: ForecastHourData[];
+}) => {
+  // Calculate the min and max SWELLHGT values from dataPoints
+  const rawMinHeight = Math.min(...dataPoints.map((item) => item.SWELLHGT));
+  const rawMaxHeight = Math.max(...dataPoints.map((item) => item.SWELLHGT));
+
+  const minHeight = parseFloat(rawMinHeight.toFixed(1));
+  const maxHeight = parseFloat(rawMaxHeight.toFixed(1));
+
+  // Function to compute the height of each bar based on the SWELLHGT value
+  const computeHeight = (swellHeight: number) => {
+    if (swellHeight >= 3) {
+      return 40;
+    }
+    // Proportional height calculation
+    return (40 * swellHeight) / 3;
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.barChartContainer}>
+        {dataPoints.map((item) => (
+          <View
+            key={item.id}
+            style={[styles.barChart, { height: computeHeight(item.SWELLHGT) }]}
+          ></View>
+        ))}
+      </View>
+      <View style={styles.heightInfo}>
+        <Text style={styles.heightInfoText}>
+          {minHeight}-{maxHeight}
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ForecasHorizontalChart;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  barChartContainer: {
+    flexDirection: "row",
+    alignItems: "flex-end",
+    gap: 1,
+    height: 40,
+  },
+  barChart: {
+    width: 10,
+    backgroundColor: Colors.logo,
+  },
+  heightInfo: {
+    backgroundColor: Colors.logo,
+    width: 45,
+    height: 40,
+    paddingTop: 2,
+    borderTopColor: "white",
+    borderTopWidth: 1,
+  },
+  heightInfoText: {
+    color: "white",
+    fontSize: 12,
+  },
+});
--- components/ForecastData/ForecastHorizontalDay.tsx
+++ components/ForecastData/ForecastHorizontalDay.tsx
@@ -1,22 +1,47 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { ForecastDayData } from "../../types/forecast";
+import ForecasHorizontalChart from "./ForecasHorizontalChart";
+import { Colors } from "../../constants/styles";
 
 const ForecastHorizontalDay = ({
   forecastData,
 }: {
   forecastData: ForecastDayData;
 }) => {
+  let dateObject = new Date(forecastData.date); // Create a date object from the string
+  // Get the name of the weekday
+  let weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+  let day = weekday[dateObject.getDay()]; // This will give you the day of the week
+  let dayDate = dateObject.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "2-digit",
+  });
   const itemsToPrint = forecastData.data.filter(
     (dataPoint) =>
       dataPoint.hour === 7 || dataPoint.hour === 13 || dataPoint.hour === 19
   );
-  console.log(itemsToPrint);
+
   return (
-    <View>
-      <Text>ForecastHorizontal</Text>
+    <View style={styles.container}>
+      <Text style={styles.day}>{day}</Text>
+      <Text>{dayDate}</Text>
+      <ForecasHorizontalChart dataPoints={itemsToPrint} />
     </View>
   );
 };
 
 export default ForecastHorizontalDay;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 45,
+    borderRightWidth: 1,
+    borderRightColor: Colors.backgroundDark,
+    alignItems: "center",
+  },
+  day: {
+    fontWeight: "bold",
+  },
+});
